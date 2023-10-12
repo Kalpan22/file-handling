b52821d57fd547be5c3bc3736bf11b40febf0f77
Kalpan22 <147749849+Kalpan22@users.noreply.github.com>
2023-10-12 23:15:33 +0530

    Added table file

---
+++ table3.java
@@ -0,0 +1,30 @@
+
+public class Tableof3
+{
+
+	static int sum=0;
+	
+	public static void main(String[] args) {
+		for(int row=1; row<=4;row++)
+		{
+			for(int column=1;column<=4;column++)
+			{
+				if(row%2==0)
+				{
+					
+					System.out.print(sum+" ");
+					sum=sum+3;
+				}
+				//if(row%2==1)
+				else
+				{
+					
+					System.out.print(sum+" ");
+					sum=sum+3;
+				}
+			}
+			System.out.println();
+		}
+	}
+	
+}
